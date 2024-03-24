2c8f73eea455707ec1b5a15e31cf5ef9f5623b60
NIKHIL <122862339+HELLNIX1@users.noreply.github.com>
2024-03-24 15:41:23 +0530

    BASICS

--- PROJECT1/settings.py
+++ PROJECT1/settings.py
@@ -36,10 +36,17 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
+    'debug_toolbar'
 ]
-
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 MIDDLEWARE = [
+    
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'PROJECT1.urls'
--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# BREAKPOINT EXCECUTES TILL THAT , THEN LINE BY LINE
+def cal():
+    x = 1
+    y = 2
+    return x
+    
 def hello(request):
+    x = cal()
     return render(request,'HELLO.html',{'name' : 'NIK'})
     # return
     
