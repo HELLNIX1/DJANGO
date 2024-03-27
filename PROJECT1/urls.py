becc99283f1ce7060d84363891a4cbc87bc3b1a6
NIKHIL <122862339+HELLNIX1@users.noreply.github.com>
2024-03-27 23:25:40 +0530

    urls

--- PROJECT1/urls.py
+++ PROJECT1/urls.py
@@ -17,9 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('base')),
+    path('',include('base.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
